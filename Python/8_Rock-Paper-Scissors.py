23a625e5b7bcc3303a95733d7530a672ef9c99bb
Onome <onome.aniyeye@gmail.com>
2022-06-10 23:17:19 +0100

    Rock, Paper, Scissors game

--- Python/8_Rock-Paper-Scissors.py
+++ Python/8_Rock-Paper-Scissors.py
@@ -1,8 +1,62 @@
-user_input = str(input("enter your choice: "))
-possible_choices = ["Rock","Paper","Scissors"]
+import random
 
-if user_input != items in possible_choices: #user_input == possible_choices[0] or possible_choices[1] or possible_choices[2]:   
-    print ("You have chosen", user_input)
+def circleBack():
+    while True:
+        user_input = input("Enter your choice: ")
+        if user_input in smallLetter:
+            print("Choice need to be in capital letters. \n Ensure to enter R for Rock, P for Paper and S for Scissors")
+            continue
+
+        elif not user_input in possible_options:
+            print("Wrong Input. Please try again. \n Ensure to enter R for Rock, P for Paper and S for Scissors")
         
-else: #user_input != items in possible_choices:
-    print ("Kindly make a correct choice")
+            continue
+    
+        else:
+            computer_action = random.choice(possible_options)
+            print(f"\nYou chose {user_input}, computer chose {computer_action}.\n")
+            #Correct input found.
+            
+            return (user_input)
+
+
+print("Welcome to Nomzee Rock, Paper, Scissors Game. \n Please enter R for Rock, P for Paper and S for Scissors")
+possible_options = ["R","P","S"]
+smallLetter = ["r", "p", "s"]
+while True:
+    user_input = input("Enter your choice: ")
+    if user_input in smallLetter:
+        print("Choice need to be in capital letters. \n Ensure to enter R for Rock, P for Paper and S for Scissors")
+        continue
+
+    elif not user_input in possible_options:
+        print("Wrong Input. Please try again. \n Ensure to enter R for Rock, P for Paper and S for Scissors")
+    
+        continue
+    
+    else:
+        #Correct input found.
+        break
+
+computer_action = random.choice(possible_options)
+print(f"\nYou chose {user_input}, computer chose {computer_action}.\n")
+
+if user_input == computer_action:
+        print(f"Both players selected {user_input}. It's a tie!")
+        circleBack()
+
+if user_input == "R":
+    if computer_action == "S":
+        print("Rock smashes Scissors! You win!")
+    else:
+        print("Paper covers Rock! Computer wins!")
+elif user_input == "P":
+    if computer_action == "R":
+        print("Paper covers Rock! You win!")
+    else:
+        print("Scissors cuts Paper! Computer wins!")
+elif user_input == "S":
+    if computer_action == "P":
+        print("Scissors cuts Paper! You win!")
+    else:
+        print("Rock smashes Scissors! Computer wins!")

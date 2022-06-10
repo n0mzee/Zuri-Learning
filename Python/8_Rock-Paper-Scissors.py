ef630de45a7603d82725926232deafedb0f82dda
Onome <onome.aniyeye@gmail.com>
2022-06-10 14:13:27 +0100

    setting up django environment

---
+++ Python/8_Rock-Paper-Scissors.py
@@ -0,0 +1,8 @@
+user_input = str(input("enter your choice: "))
+possible_choices = ["Rock","Paper","Scissors"]
+
+if user_input != items in possible_choices: #user_input == possible_choices[0] or possible_choices[1] or possible_choices[2]:   
+    print ("You have chosen", user_input)
+        
+else: #user_input != items in possible_choices:
+    print ("Kindly make a correct choice")
